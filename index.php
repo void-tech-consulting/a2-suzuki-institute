54a687498d32e7b7fe7f9e71a10a4ba011551dda
jfaro <jfaro@umich.edu>
2020-12-04 14:34:03 -0500

    prepped index.php

--- index.php
+++ index.php
@@ -2,9 +2,15 @@
   get_header();
   require get_template_directory() . '/inc/section_vars.php';
 ?>
-<h1>The Front Page</h1>
+<h1>Home</h1>
 
-<?php if (get_theme_mod($home_top_img)) { ?>
+
+
+
+
+
+
+<!-- <?php if (get_theme_mod($home_top_img)) { ?>
   <img 
     src="<?php echo get_theme_mod($home_top_img) ?>" 
     alt=""
@@ -15,14 +21,6 @@
   <h4>
     <?php echo get_theme_mod($home_top_desc) ?>
   </h4>
-<?php } ?>
-
-<!-- Example with default video if $home_top_vid isn't set -->
-<iframe 
-  class="yt-vid" 
-  src="<?php echo get_theme_mod($home_top_vid, 'https://www.youtube.com/embed/A0Wyx-OOX4A'); ?>" 
-  frameborder="0" 
-  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-</iframe>
+<?php } ?> -->
 
 <?php get_footer(); ?>
