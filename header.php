2691bbfd32fdcc5f96043c51a6d008b75b562c34
jfaro <jfaro@umich.edu>
2020-12-04 14:33:41 -0500

    added header skeleton

--- header.php
+++ header.php
@@ -5,17 +5,36 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>My Example Website Title</title>
+  <title>Ann Arbor Suzuki Institute</title>
 </head>
 <body>
 <!-- Example navbar -->
-  <div id="menu">
-    <?php  
-    $args = array(
-      "theme_location" => "primary",
-      "container" => "ul",
-      "menu_class" => "nav-links"
-    );
-    wp_nav_menu( $args);
-    ?>
+<div id="header-container">
+
+<!-- Contact Info / Alert message -->
+  <div id="header-banner">
+    <span><pre>(734) 726-0171    annarborsuzuki@gmail.com</pre></span>
+    <span>Online lessons available!</span>
   </div>
+
+  <!-- Title / Nav -->
+  <div id="header-main">
+    <span id="header-left">Ann Arbor Suzuki Institute</span>
+    <div id="header-right">
+      <a href="/home">Home</a>
+    </div>
+  </div>
+  
+</div>
+
+<!-- <div id="menu">
+  <?php  
+  $args = array(
+    "theme_location" => "primary",
+    "container" => "ul",
+    "menu_class" => "nav-links"
+  );
+  wp_nav_menu( $args);
+  ?> -->
+</div>
+  
