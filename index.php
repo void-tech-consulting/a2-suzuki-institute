d70ca85ea195820ce39210cb8f868d19dff834ee
lailajs <lailajs@umich.edu>
2020-12-26 16:43:03 -0500

    first assignment

--- index.php
+++ index.php
@@ -7,7 +7,21 @@
 
 
 
-
+<!-- Photo gallery -->
+<header>Photo Gallery</header>
+<div class="row">
+        <div class="column">
+          <img src= "img1.png">
+          <img src= "img2.png"> 
+          <img src="img3.png">
+        </div>
+        
+        <div class="column">
+          <img src="img4.png">
+          <img src="img5.png">
+          <img src="">
+        </div>
+      </div>
 
 
 <!-- <?php if (get_theme_mod($home_top_img)) { ?>
