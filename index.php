1bfebfaf4af674e11b9a8ce3309d6972eb0cdddf
shaanp <shaanp@umich.edu>
2020-12-26 19:16:11 -0500

    Styling the love hear message box

--- index.php
+++ index.php
@@ -20,15 +20,33 @@ require get_template_directory() . '/inc/section_vars.php';
 
     <div class="page-contact-container">
 
-      <p class="contact-contact">Contact</p>
+      <p class="contact-contact-text">Contact</p>
       <hr class="contact-horizontal-bar">
       </hr>
 
-      <label for="name" class="contact-text">Name</label>
-      <br>
-      <input class="contact-input-box" type="text" id="name" name="name" placeholder="Enter your name">
-    </div>
+      <div class="contact-boxes">
+        <div class="contact-fields">
+          <label for="name" class="contact-text">Name</label>
+          <br>
+          <input class="contact-input-box" type="text" id="name" name="name" placeholder="Enter your name">
+          <br>
+          <label for="emailPhone" class="contact-text">Email / Phone</label>
+          <br>
+          <input class="contact-input-box" type="text" id="emailPhone" name="emailPhone" placeholder="Enter your email address or phone">
+          <br>
+          <label for="message" class="contact-text">Message</label>
+          <br>
+          <input class="contact-input-box" style="height: 200px; padding-top: 10px; padding-right: 143px; padding-bottom: 154px" type="text" id="message" name="message" placeholder="Enter a message">
+          <br>
+        </div>
 
+        <div class="contact-message-container">
+          <div class="contact-message">
+            <p class="contact-loveHear-heading-text">We'd love to hear from you!</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
 
