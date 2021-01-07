c30a2f045b449778cfa3608d6e468d04cff8f217
imanshananawani <imans@umich.edu>
2021-01-07 13:17:33 -0500

    footer and testimonials

--- footer.php
+++ footer.php
@@ -1,2 +1,12 @@
 <!-- If you have html on the footer of each page put it here -->
 <?php wp_footer(); ?>
+
+<!-- Footer -->
+<script src="https://unpkg.com/ionicons@5.2.3/dist/ionicons.js"></script>
+<div class="footer">
+    <h2 class="icon"> <ion-icon name="logo-facebook"></ion-icon></h2>
+    <div class="footerlinks">
+       <div class="PhoneNumber"> <h2>(734) 726-0171</h2></div>
+       <div class="Email"> <h2>annarborsuzuki@gmail.com</h2></div>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -15,7 +15,47 @@
 
 
   <!-- Testimonial -->
-
+  <h1 class= "header"> 
+  Testimonials
+  </h1>
+<div class = "testimonial"> 
+  <div class= "writing">
+    <p>
+    “Lorem ipsum dolor sit amet,
+    consectetur adipiscing elit.
+    Phasellus euismod semper dolor, a
+    hendrerit odio placerat nec. Nunc
+    massa massa, faucibus congue
+    massa commodo, sodales pretium
+    tellus. “
+    </p>
+    <h1 class= "parent"> -Parent </h1>
+  </div>
+  <div class= "writing">
+    <p>
+    “Lorem ipsum dolor sit amet,
+    consectetur adipiscing elit.
+    Phasellus euismod semper dolor, a
+    hendrerit odio placerat nec. Nunc
+    massa massa, faucibus congue
+    massa commodo, sodales pretium
+    tellus. “
+    </p>
+    <h1 class= "parent"> -Parent </h1>
+  </div>
+  <div class= "writing">
+    <p>
+    “Lorem ipsum dolor sit amet,
+    consectetur adipiscing elit.
+    Phasellus euismod semper dolor, a
+    hendrerit odio placerat nec. Nunc
+    massa massa, faucibus congue
+    massa commodo, sodales pretium
+    tellus. “
+    </p>
+    <h1 class= "parent"> -Parent </h1>
+  </div>
+</div>
 
   </div>
 </div>
