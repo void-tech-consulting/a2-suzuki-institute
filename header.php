eac7958510e649caf1b076a5986b34657a3f7a5e
jfaro <jfaro@umich.edu>
2021-01-24 22:14:56 -0500

    added header with background image

--- header.php
+++ header.php
@@ -2,35 +2,46 @@
 <?php wp_head(); ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Ann Arbor Suzuki Institute</title>
 </head>
+
 <body>
-<!-- Example navbar -->
-<div id="header-container">
+  <!-- Example navbar -->
+  <div id="header-container">
 
-<!-- Contact Info / Alert message -->
-  <div id="header-banner">
-    <span><pre>(734) 726-0171    annarborsuzuki@gmail.com</pre></span>
-    <span>Online lessons available!</span>
+    <!-- background image -->
+    <div id="header-background">
+      <img src="<?php echo get_bloginfo('template_url') ?>/images/jumbotron-background.png" />
+    </div>
+
+    <!-- Contact Info / Alert message -->
+    <div id="header-banner">
+      <span>
+        <pre>(734) 726-0171    annarborsuzuki@gmail.com</pre>
+      </span>
+      <span>Online lessons available!</span>
+    </div>
+
+    <!-- Title / Nav -->
+    
+    <div id="header-main">
+      <span id="header-main-left">Ann Arbor Suzuki Institute</span>
+      <div id="header-main-right">
+        <?php
+        $args = array(
+          "theme_location" => "primary",
+          "container" => "ul",
+          "menu_class" => "nav-links"
+        );
+        wp_nav_menu($args);
+        ?>
+      </div>
+    </div>
+
+
+
   </div>
-
-  <!-- Title / Nav -->
-  <div id="header-main">
-    <span id="header-main-left">Ann Arbor Suzuki Institute</span>
-    <div id="header-main-right">
-    <?php  
-      $args = array(
-        "theme_location" => "primary",
-        "container" => "ul",
-        "menu_class" => "nav-links"
-      );
-      wp_nav_menu( $args);
-    ?>
-  </div>
-</div>
-
-</div>
-  
--- index.php
+++ index.php
@@ -5,18 +5,25 @@ require get_template_directory() . '/inc/section_vars.php';
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
 
-<div class="page-container">
+<div class="home-jumbotron-content">
+  <span class="secondary-header-text shadow-hard">Excellent Music Instruction</span>
+  <p class="shadow-hard">Serving the Ann Arbor, Michigan area for over 45 years</p>
+  <div class="vertical-spacer"></div>
+  <button>Contact</button>
+  <div class="vertical-spacer"></div>
+  <div class="vertical-spacer"></div>
+</div>
+
+<div class="page-container" style="top: 580px">
   <div class="page-content-container">
 
     <!-- About -->
-
+    <p class="page-header-text">About</p>
 
     <!-- Photo gallery -->
-    
-  <!-- Buttons -->
-  <button> button </button>
-  <button class = "btn-outline">btn</button>
-  
+
+    <!-- Buttons -->
+
     <!-- Testimonial -->
 
   </div>
