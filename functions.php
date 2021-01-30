64e3eb845b8c7ea00c6bc2fffffbab5711d17caa
jfaro <jfaro@umich.edu>
2021-01-30 11:08:53 -0500

    updated functions.php

--- functions.php
+++ functions.php
@@ -2,6 +2,8 @@
   require get_template_directory() . '/inc/example-post-type.php';
   require get_template_directory() . '/inc/enqueue-scripts.php';
   require get_template_directory() . '/inc/register-settings.php';
+
+  
 ?>
 
 <!-- // Add Home page section to admin appearance customize screen
