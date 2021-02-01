c530817697602448aeeb6383f8f0f29edfc9b8dc
jfaro <jfaro@umich.edu>
2021-01-31 23:56:17 -0500

    update to functions.php

--- functions.php
+++ functions.php
@@ -5,8 +5,3 @@
   require get_template_directory() . '/inc/customizer.php';
   require get_template_directory() . '/inc/template_functions.php';
 ?>
-
-<!-- // Add Home page section to admin appearance customize screen
-function atwosi_home_section() {
-
-} -->
