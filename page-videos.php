d6a18618ae5fd0813e46cb1d4da1d0f71197e344
jfaro <jfaro@umich.edu>
2021-02-04 16:36:58 -0500

    updated videos page

--- page-videos.php
+++ page-videos.php
@@ -6,40 +6,38 @@
 
         <div class="page-section vertical-stack">
             <div class="video-row">
-            <div class="video-text">
-                <h3>Video Title</h3>
-                <p>Video description. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus euismod semper dolor, a hendrerit odio placerat nec. Nunc massa massa, faucibus congue massa commodo, sodales pretium tellus.</p>
+                <div class="video-text">
+                    <h3>Video Title</h3>
+                    <p>Video description. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus euismod semper dolor, a hendrerit odio placerat nec. Nunc massa massa, faucibus congue massa commodo, sodales pretium tellus.</p>
+                </div>
+
+                <div class="video-thumbnail">
+                    <img src="https://i.imgur.com/h0wHByK.png">
+                </div>
             </div>
 
-            <div class="video-thumbnail">
-                <img src="https://i.imgur.com/h0wHByK.png">
+            <div class="video-row">
+                <div class="video-text">
+                    <h3>Video Title</h3>
+                    <p>Video description. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus euismod semper dolor, a hendrerit odio placerat nec. Nunc massa massa, faucibus congue massa commodo, sodales pretium tellus.</p>
+                </div>
+
+                <div class="video-thumbnail">
+                    <img src="https://i.imgur.com/h0wHByK.png">
+                </div>
+            </div>
+
+            <div class="video-row">
+                <div class="video-text">
+                    <h3>Video Title</h3>
+                    <p>Video description. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus euismod semper dolor, a hendrerit odio placerat nec. Nunc massa massa, faucibus congue massa commodo, sodales pretium tellus.</p>
+                </div>
+
+                <div class="video-thumbnail">
+                    <img src="https://i.imgur.com/h0wHByK.png">
+                </div>
             </div>
         </div>
-
-        <div class="video-row">
-            <div class="video-text">
-                <h3>Video Title</h3>
-                <p>Video description. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus euismod semper dolor, a hendrerit odio placerat nec. Nunc massa massa, faucibus congue massa commodo, sodales pretium tellus.</p>
-            </div>
-
-            <div class="video-thumbnail">
-                <img src="https://i.imgur.com/h0wHByK.png">
-            </div>
-        </div>
-
-        <div class="video-row">
-            <div class="video-text">
-                <h3>Video Title</h3>
-                <p>Video description. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus euismod semper dolor, a hendrerit odio placerat nec. Nunc massa massa, faucibus congue massa commodo, sodales pretium tellus.</p>
-            </div>
-
-            <div class="video-thumbnail">
-                <img src="https://i.imgur.com/h0wHByK.png">
-            </div>
-        </div>
-        </div>
-        
-    </div>
 
 <?php 
   get_footer(); 
