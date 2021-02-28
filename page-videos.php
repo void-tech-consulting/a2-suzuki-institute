0e89ff10783b872aaf23c51d536df0b9f385d6bc
jfaro <jfaro@umich.edu>
2021-02-28 17:06:48 -0500

    videos changes

--- page-videos.php
+++ page-videos.php
@@ -11,29 +11,27 @@
             // get_example_data is in /inc/template_functions.php
             $data = get_videos_data($videos_repeater);
             if (!empty($data)) {
-            ?>
-            <?php foreach ($data as $k => $f) {
-                    echo '<div class="video-row">';
-                    echo '<div class="video-text">';
-                    $video_title = '<h3>' . $f['title'] . '</h3>';
-                    $video_desc = '<p>' . $f['description'] . '</p>';
-                    echo $video_title;
-                    echo $video_desc;
-                    echo '</div>';
+                foreach ($data as $k => $f) { ?>
+                    <div class="video-row">
+                        <div class="video-text">
+                            <h3><?php echo $f['title'] ?></h3>
+                            <p><?php echo $f['description'] ?></p>
+                        </div>
+                        <div class="video-thumbnail">
+                            <video width="320" height="240" controls>
+                                <?php
+                                $media = '<source src="' . esc_url(get_media_url($f['video'])) . '" type=video/mp4> </video>';
+                                ?>
+                                <?php echo $media ?>
+                        </div>
+                    </div>
 
-                    echo '<div class="video-thumbnail">';
-                    $video = '<video width="320" height="240" controls>
-                                        <source src="' . esc_url(get_media_url($f['video'])) .
-                        '" type=video/mp4> </video>';
-                    echo $video;
-                    echo '</div>';
-
-                    echo '</div>';
-
-                    $media = '<img src="' . esc_url(get_media_url($f['image'])) . '" alt="' . $image_alt . '">';
-                }
-            }
-            ?>
+                <?php } ?>
+            <?php } else { ?>
+                <div class="video-row">
+                    <p>No videos have been uploaded</p>
+                </div>
+            <?php } ?>
         </div>
 
         <?php
