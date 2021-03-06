638ce5431364a8f2012b3986f56e2027c14ae106
shaanp <shaanp@umich.edu>
2021-03-05 22:53:15 -0500

    Created event page in Wordpress

---
+++ page-events.php
@@ -0,0 +1,13 @@
+<?php /* Template Name: events */ ?>
+<?php get_header(); ?>
+<div class="page-container">
+    <div class="page-content-container">
+        <h2 class="page-header-text">Events</h2>
+
+        <div class="page-section vertical-stack">
+
+        </div>
+
+        <?php
+        get_footer();
+        ?>
