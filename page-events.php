7fab895f5ab15a679d2bd6c24ccde6c48c72ca6d
shaanp <shaanp@umich.edu>
2021-03-06 20:10:16 -0500

    Event calendar box looks good

--- page-events.php
+++ page-events.php
@@ -6,6 +6,28 @@
 
         <div class="page-section vertical-stack">
 
+            <div class="event-row">
+
+                <div class="event-box">
+                    <div class="rectangle-1">
+                        <p class="month-text">Month</p>
+                        <div class="rectangle-2">
+                            <div class="rectangle-3-row">
+                                <div class="rectangle-3-column">
+                                    <p class="day-text">Day</p>
+                                    <p class="day-text">#</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="event-info">
+                <p class="event-title-text">Name/Title</p>
+                </div>
+
+            </div>
+
         </div>
 
         <?php
